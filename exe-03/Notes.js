45ddef926b6ac0f5eb9df61c55efac0af674651a
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-09-25 15:57:18 -0300

    adicionado variáveis no constructor() e melhorando visualização das notas

--- exe-03/Notes.js
+++ exe-03/Notes.js
@@ -30,6 +30,12 @@ async function validateAnswer(question, error, screenMessage='') {
 
 class Notes {
 
+    constructor() {
+        this.notesPath     = path.resolve(__dirname, 'notes')
+        this.notes         = fs.readdirSync(this.notesPath)
+        this.quantityNotes = this.notes.length
+    }
+
     contentNote() {
         const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
@@ -55,48 +61,67 @@ class Notes {
         let filename = await askToUser('Informe o nome do arquivo: ')
         const content  = await this.contentNote()
 
-        const filePath = path.resolve(__dirname, 'notes')
-        let quantityNotes = fs.readdirSync(filePath).length
+        let quantityNotes
         
-        quantityNotes = quantityNotes < 2 ? `0${quantityNotes}` : quantityNotes
+        quantityNotes = this.quantityNotes < 2 ? `0${this.quantityNotes}` : this.quantityNotes
         
-        if (!fs.existsSync(filePath)) {
-            fs.mkdirSync(filePath)
+        if (!fs.existsSync(this.notesPath)) {
+            fs.mkdirSync(this.notesPath)
         }
 
         filename == '' ? filename = `note_${quantityNotes}` : filename
 
         new File(
-            path.join(filePath, `${filename}.txt`)
+            path.join(this.notesPath, `${filename}.txt`)
         ).write( content )
     }
 
     async deleteNote() {
         console.clear()
         
-        this.listNotes()
+        const notesList = this.listNotes()
 
-        const filename = await validateAnswer('Informe o nome do arquivo que deseja deletar: ', 'Por favor, informe o nome do arquivo que deseja deletar!')
+        console.log(notesList)
+        const filename = await validateAnswer('Informe o nome do arquivo que deseja deletar: ', 'Por favor, informe o nome do arquivo que deseja deletar!', notesList)
 
-        const filePath = path.resolve(__dirname, 'notes')
         new File(
-            path.join(filePath, `${filename}.txt`)
+            path.join(this.notesPath, `${filename}.txt`)
         ).delete()
     }
 
 
     listNotes() {
         console.clear()
-        const notesPath = path.resolve(__dirname, 'notes')
 
-        if (!fs.existsSync(notesPath)) {
+        if (!fs.existsSync(this.notesPath)) {
             console.log('A pasta notes ainda não foi criada')
         }
 
-        const notes = fs.readdirSync(notesPath)
-        console.log('=-=-=-=-=-=- Lista de notas -=-=-=-=-=-=\n')
-        notes.forEach( (note, id) => console.log(`[ ${id+1} ] ${note.replace('.txt', '')}`) )
-        console.log('\n=-=-=-=-=-=-=-=-=-=- =-=-=-=-=-=-=-=-=-=\n')
+        const notes = fs.readdirSync(this.notesPath)
+        let notesList = '=-=-=-=-=-=- Lista de notas -=-=-=-=-=-=\n'
+        notes.forEach( (note, id) => notesList += `\n[ ${id+1} ] ${note.replace('.txt', '')}\n` )
+        notesList += '\n=-=-=-=-=-=-=-=-=-=- =-=-=-=-=-=-=-=-=-=\n'
+
+        return notesList
+    }
+
+    async showNote() {
+        console.clear()
+        const notesList = this.listNotes()
+
+        console.log(notesList)
+        let note = await validateAnswer('Escolha uma das notas acima para visualizar: ', 'Por favor, informe uma opção!', notesList)
+
+        
+        if ( parseInt(note) ) {
+            note = this.notes[ parseInt(note) - 1 ].replace('.txt', '')
+        }
+        
+        console.clear()
+        new File(
+            path.join(this.notesPath, note + '.txt')
+        ).show()
+
 
     }
 }
