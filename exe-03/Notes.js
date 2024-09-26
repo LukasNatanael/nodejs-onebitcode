dc62108fbb02c53f85d145b182aa6194b6d00089
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-09-26 14:37:02 -0300

    adicionado updateNote() e alterado váriáveis da classe

--- exe-03/Notes.js
+++ exe-03/Notes.js
@@ -36,12 +36,23 @@ class Notes {
         this.quantityNotes = this.notes.length
     }
 
-    contentNote() {
+    validateOption(note) {
+        if ( parseInt(note) ) {
+            note = this.notes[ parseInt(note) - 1 ]
+        }
+        else {
+            note = note + '.txt'
+        }
+
+        return note
+    }
+
+    contentNote(question='') {
         const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
         console.clear()
         return new Promise((resolve) => {
-            rl.question('Informe abaixo o que deseja anotar: \n', (answer) => {
+            rl.question(question, (answer) => {
                 rl.on('line', (data) => {
                     answer += `\n${data}`
                 })
@@ -59,7 +70,7 @@ class Notes {
         console.clear()
         // const filename = await validateAnswer('Informe o nome do arquivo: ', 'Por favor, informe o nome do arquivo!')
         let filename = await askToUser('Informe o nome do arquivo: ')
-        const content  = await this.contentNote()
+        const content  = await this.contentNote('Informe abaixo o que deseja anotar: \n')
 
         let quantityNotes
         
@@ -82,10 +93,12 @@ class Notes {
         const notesList = this.listNotes()
 
         console.log(notesList)
-        const filename = await validateAnswer('Informe o nome do arquivo que deseja deletar: ', 'Por favor, informe o nome do arquivo que deseja deletar!', notesList)
+        let filename = await validateAnswer('Informe o nome do arquivo que deseja deletar: ', 'Por favor, informe o nome do arquivo que deseja deletar!', notesList)
 
+        filename = this.validateOption(filename)
+        
         new File(
-            path.join(this.notesPath, `${filename}.txt`)
+            path.join(this.notesPath, filename)
         ).delete()
     }
 
@@ -112,16 +125,37 @@ class Notes {
         console.log(notesList)
         let note = await validateAnswer('Escolha uma das notas acima para visualizar: ', 'Por favor, informe uma opção!', notesList)
 
-        
-        if ( parseInt(note) ) {
-            note = this.notes[ parseInt(note) - 1 ].replace('.txt', '')
-        }
+        note = this.validateOption(note)
         
         console.clear()
         new File(
-            path.join(this.notesPath, note + '.txt')
+            path.join(this.notesPath, note)
         ).show()
 
+
+    }
+
+    async updateNote() {
+
+        console.clear()
+        const notesList = this.listNotes()
+
+        console.log(notesList)
+        let note = await validateAnswer('Escolha uma das notas acima para editar: ', 'Por favor, informe uma opção!', notesList)
+
+        
+        note = validateOption(note)
+
+        
+        console.clear()
+        
+        note = new File(path.join(this.notesPath, note))
+
+        note.show()
+        
+        const content  = await this.contentNote()
+
+        note.update(`\n${content}`)
 
     }
 }
