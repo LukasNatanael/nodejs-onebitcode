cba635ce4218ccbb8aec089a8b590a4929860bef
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-10-05 15:01:00 -0300

    criado arquivo responsável por gerenciar rotas

---
+++ aplicação-web-nodejs/05-projeto-MVC-blog/src/views/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const postController = require('../controllers/postController')
+
+const router = express.Router()
+
+router.get('/', postController.index)
+router.get('/posts/:id', postController.show)
+
+module.exports = router
