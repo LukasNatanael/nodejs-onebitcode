cd9703f492b9533b281d855c47532233752b0ea8
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-10-05 14:58:54 -0300

    criado arquivo principal para rodar o server

---
+++ aplicação-web-nodejs/05-projeto-MVC-blog/src/index.js
@@ -0,0 +1,30 @@
+// importando módulos necessários
+const express = require('express')
+const path    = require('node:path')
+const router  = require('./views/routes')
+
+
+// criando servidor
+const app = express()
+
+// definindo porta, irá utilizar da váriavel de ambiente caso possua senão, irá utilziar a porta 3000 como padrão
+const PORT = process.env.PORT || 3000
+
+// configurando EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// configuração de arquivos estáticos
+app.use( express.static('public') )
+
+// configuração para ler dados das requisições
+app.use( express.urlencoded({ extended: true }) )
+
+// rotas da aplicação
+app.use(router)
+
+
+// rotas para POSTS
+
+console.clear()
+app.listen(PORT, console.log(`Servidor rodando em http://localhost:${PORT}`))
