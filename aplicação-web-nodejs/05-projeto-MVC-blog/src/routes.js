a3bbb413e50a16d9802f9cb007ec6f2a655589d5
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-10-05 16:46:05 -0300

    adicionado rotas admin

---
+++ aplicação-web-nodejs/05-projeto-MVC-blog/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const postController = require('./controllers/postController')
+const adminController = require('./controllers/adminController')
+
+const router = express.Router()
+
+// rotas do BLOG
+router.get('/', postController.index)
+router.get('/posts/:id', postController.show)
+
+// rotas do ADMIN
+router.get('/admin', adminController.index)
+router.get('/admin/create', adminController.create) 
+router.post('/admin/create', adminController.save)
+
+router.get('/admin/edit/:id', adminController.edit)
+router.post('/admin/update/:id', adminController.update)
+
+router.post('/admin/delete/:id', adminController.delete)
+
+module.exports = router
