9b96e01d73720dddecd04d4fb75a150b086e8a49
Lukas <lukasnatanael889@gmail.com>
2024-10-27 11:32:50 -0300

    removido database route

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/index.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const path    = require('node:path')
 const routes = require('./routes')
-const db = require('@antoniosbarotsis/fake-db')
-
 
 const app = express()
 const PORT   = process.env.PORT || 3000
-const DBPORT = process.env.DBPORT || 8000
-
-
 
 app.set('view engine', 'ejs')
 app.set('views', path.join( __dirname, 'views' ))
@@ -18,7 +13,5 @@ app.use( express.urlencoded({ extended: true }) )
 app.use(routes)
 
 console.clear()
-
-db.start(DBPORT, console.log(`Database is running in: \nhttp://localhost:${DBPORT}/taskLists`))
 
 app.listen( PORT, console.log(`Server is running in: \nhttp://localhost:${PORT}`) )
