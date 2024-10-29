e62a827b1b2bfc420f8a1b27d71a649ea4cc0dbc
Lukas <lukasnatanael889@gmail.com>
2024-10-29 08:36:30 -0300

    criado tarefas de teste e importado models

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/controllers/appController.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/controllers/appController.js
@@ -1,12 +1,33 @@
-const { ToDoList } = require('../models/ToDoListModel')
+const { ToDoList } = require('../models/ListModel')
+const { TaskList } = require('../models/TaskModel')
 
-const list = new ToDoList()
-list.newList('Tarefas de casa')
-list.newList('Tarefas do trabalho')
+const toDo = new ToDoList()
+const tarefas_de_casa = new TaskList('Tarefas de casa')
+tarefas_de_casa.newTask('Varrer o chão')
+tarefas_de_casa.newTask('Dobrar roupas')
+
+const tarefas_do_trabalho = new TaskList('Tarefas do trabalho')
+tarefas_do_trabalho.newTask('Verificar grafana')
+tarefas_do_trabalho.newTask('Verificar atendimentos')
+tarefas_do_trabalho.newTask('Verificar OS')
+
+toDo.newList(tarefas_de_casa)
+toDo.newList(tarefas_do_trabalho)
+
 class AppController {
     index(req, res) {
-        const allLists = list.getAllLists()
+        const allLists = toDo.getAllLists()
         res.render('index', { list: allLists })
+    }
+
+    list(req, res) {
+        const id = req.params.id
+        const currentList = toDo.getListByID(id)
+        
+        res.render('list', { name: currentList.name, tasks: currentList.tasks._tasks })
+
+        // console.log( currentList.tasks._tasks )
+        
     }
 }
 
