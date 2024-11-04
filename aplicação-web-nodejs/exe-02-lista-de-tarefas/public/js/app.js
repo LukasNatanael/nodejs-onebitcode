89d734655b55934701cf76a340bac58c64761e39
Lukas <lukasnatanael889@gmail.com>
2024-11-04 08:00:27 -0300

    finalizado botão para criar uma nova lista

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/public/js/app.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/public/js/app.js
@@ -6,28 +6,54 @@ function generateID() {
 }
 
 function newList() {
-    const sectionList = document.querySelector('ul')
+    const listname = prompt('Informe o nome da lista: ')
+
+    if (listname === '') { return alert('Por favor, informe um nome para a lista!') }
+
+    const id = generateID()
     const ul    = document.querySelector('ul')
     const li    = document.createElement('li')
-    li.id = 'list-' + generateID()
-    
-    const input = document.createElement('input')
-    input.placeholder = 'Informe o nome da lista...'
+    li.setAttribute('id', `item-${id}`)
 
-    li.append( input )
+    const buttonsDiv = document.createElement('div')
+    buttonsDiv.classList.add('buttons-div')
+    
+    const listItem = document.createElement('form')
+    listItem.setAttribute('action', `app/list-${id}`)
+    listItem.setAttribute('method', 'get')
+    listItem.classList.add('listItem')
+    
+    const listItemSendButton = document.createElement('button')
+    listItemSendButton.setAttribute('type', 'submit')
+    listItemSendButton.classList.add('listname')
+    listItemSendButton.innerText = listname
+
+    listItem.append(listItemSendButton)
+
+    const listItemDelete = document.createElement('form')
+    listItemDelete.setAttribute('action', `app/list-delete/${id}`)
+    listItemDelete.setAttribute('method', 'post')
+    listItemDelete.classList.add('listItemDelete')
+
+    const listItemDeleteButton =document.createElement('button')
+    listItemDeleteButton.setAttribute('type', 'submit')
+    listItemDeleteButton.classList.add('delete')
+
+    const trashIcon = document.createElement('i')
+    trashIcon.classList.add('fa-solid')
+    trashIcon.classList.add('fa-trash-can')
+    
+    listItemDeleteButton.append(trashIcon)
+    listItemDelete.append( listItemDeleteButton )
+
+    buttonsDiv.append(listItem, listItemDelete)
+
+    li.appendChild(buttonsDiv)
+
     ul.append( li )
 
-    const main = document.querySelector('main')
-
-    main.append( ul )
-    // sectionList.append( ul )
 }
 
-function newTask() {
-    const buttons = document.querySelectorAll('.newTask')
-
-
-}
 
 newToDoList.addEventListener( 'click', newList )
 
