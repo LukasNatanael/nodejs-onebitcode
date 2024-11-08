1e112488fd2f6628aa2c802c06f64a5963b6bc73
Lukas <lukasnatanael889@gmail.com>
2024-11-08 08:17:57 -0300

    Alterado rota de criação de uma nova lista

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/routes.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/routes.js
@@ -6,6 +6,7 @@ const appController = new AppController()
 
 routes.get('/', appController.index)
 routes.get('/app/list-:id', appController.showList)
+routes.post('/app/list-:listID/new-list-:listname', appController.createList)
 routes.post('/app/list-:listID/task-:taskID-complete', appController.taskComplete)
 routes.post('/app/list-delete/:listID', appController.deleteList)
 
