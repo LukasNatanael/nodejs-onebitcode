f94e3d58cf99698d3b3d21bc9169ff4303939ced
Lukas <lukasnatanael889@gmail.com>
2024-11-14 08:28:56 -0300

    atualizado rotas

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/routes.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/routes.js
@@ -6,8 +6,11 @@ const appController = new AppController()
 
 routes.get('/', appController.index)
 routes.get('/app/list-:id', appController.showList)
-routes.post('/app/list-:listID/new-list-:listname', appController.createList)
+routes.post('/app/new-list', appController.createList)
 routes.post('/app/list-:listID/task-:taskID-complete', appController.taskComplete)
 routes.post('/app/list-delete/:listID', appController.deleteList)
+
+routes.post('/app/list-:listID/new-task', appController.createTask)
+routes.post('/app/list-:listID/task-:taskID/delete', appController.deleteTask)
 
 module.exports = routes
