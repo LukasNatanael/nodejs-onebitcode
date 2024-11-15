95e0757a9e8d9201e6143bb5fec71154f94fe9a6
Lukas <lukasnatanael889@gmail.com>
2024-11-15 09:53:44 -0300

    Removido testes e comentários. Alterado forma de visualização de status

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/models/ListModel.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/models/ListModel.js
@@ -1,8 +1,5 @@
-const { TaskList } = require('../models/TaskModel')
-
 class ToDoList {
     constructor() {
-        // this._lists = {}
         this._lists = []
     }
 
@@ -18,25 +15,20 @@ class ToDoList {
     newList( list, id=this.#generateID ) {
         this._lists.push( { id: id, name: list._name, tasks: list, complete: false } )
         
+        console.log(`[ LISTA ] "${list._name}" foi criada com sucesso! \n`)
     }
 
     getListByID(id) {
+        const list = this._lists.filter( item => item.id === id )[0]
         
-        return this.getAllLists().filter( element => element.id === id )[0]
-    }
-
-    completeList(id) {
-        const listToComplete = this.getListByID(id)
-        listToComplete['complete'] = true
-
-        console.log(`[ LISTA ] "${listToComplete._name}" foi marcada como finalizada \n`)
+        return list === undefined ? `[ Lista não encontrada ] A lista solicitada não foi localizada.` : list
     }
     
     deleteList(id) {
         const taskToDelete = this.getListByID(id)
-        console.log(`[ LISTA ] "${ taskToDelete.name }" foi deletada \n`)
         this._lists = this._lists.filter( list => list.id != id )
-        
+
+        console.log(`[ LISTA ] "${ taskToDelete.name }" foi deletada! \n`)
     }
 
     checkCompleteList(id) {
@@ -45,27 +37,9 @@ class ToDoList {
         currentList.complete = tasks.checkCompleteAllTasks() === true ? true: false
         
         if (currentList.complete) {
-            console.log(`[ LISTA ] "${currentList.name}" foi marcada como finalizada \n`)
+            console.log(`[ LISTA ] "${currentList.name}" foi marcada como finalizada! \n`)
         }
-
     }
-    
 }
-
-const toDo = new ToDoList()
-const tarefas_de_casa = new TaskList('Tarefas de casa')
-tarefas_de_casa.newTask('Varrer o chão')
-tarefas_de_casa.newTask('Dobrar roupas')
-
-const tarefas_do_trabalho = new TaskList('Tarefas do trabalho')
-tarefas_do_trabalho.newTask('Verificar grafana')
-tarefas_do_trabalho.newTask('Verificar atendimentos')
-
-toDo.newList(tarefas_de_casa)
-toDo.newList(tarefas_do_trabalho)
-
-// console.log( toDo.getAllLists() )
-// console.log( toDo.getListByID() )
-
 
 module.exports = { ToDoList }
