f78733981c555e14e05337e4ffcc5602ad629151
Lukas <lukasnatanael889@gmail.com>
2024-11-15 10:13:39 -0300

    removido tarefas de teste

--- aplicação-web-nodejs/exe-02-lista-de-tarefas/src/controllers/appController.js
+++ aplicação-web-nodejs/exe-02-lista-de-tarefas/src/controllers/appController.js
@@ -2,19 +2,6 @@ const { ToDoList } = require('../models/ListModel')
 const { TaskList } = require('../models/TaskModel')
 
 const toDo = new ToDoList()
-const tarefas_de_casa = new TaskList('Tarefas de casa')
-tarefas_de_casa.newTask('Varrer o chão')
-tarefas_de_casa.newTask('Dobrar roupas')
-
-const tarefas_do_trabalho = new TaskList('Tarefas do trabalho')
-tarefas_do_trabalho.newTask('Verificar grafana')
-tarefas_do_trabalho.newTask('Verificar atendimentos')
-tarefas_do_trabalho.newTask('Verificar OS')
-
-toDo.newList(tarefas_de_casa)
-toDo.newList(tarefas_do_trabalho)
-
-console.clear()
 class AppController {
     index(req, res) {
         const allLists = toDo.getAllLists()
