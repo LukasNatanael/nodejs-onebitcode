500c478832ad5ee915e990b12277574006781582
Lukas <lukasnatanael889@gmail.com>
2024-11-27 08:30:55 -0300

    criado rotas e as padronizando

--- aplicação-web-nodejs/exe-03-API-de-playlists/src/routes.js
+++ aplicação-web-nodejs/exe-03-API-de-playlists/src/routes.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const PlaylistController = require('./controllers/playlistController')
 const routes  = express.Router() 
 
-routes.get('/', (req, res) => { res.json({ message: 'Hello World!' })  })
+const playlistController = new PlaylistController()
+
+routes.get('/', playlistController.show)
+routes.get('/playlist', playlistController.createPlaylist)
+
+// Dados com JSON
+// routes.post('/playlist', playlistController.createPlaylist)
+
+// Dados com PARÂMETROS
+routes.post('/playlist/:name/:tags', playlistController.savePlaylist)
+routes.get('/playlist/:name/:tags', playlistController.show)
+
+routes.delete('/playlist/:id', playlistController.deletePlaylist)
 
 module.exports = routes
