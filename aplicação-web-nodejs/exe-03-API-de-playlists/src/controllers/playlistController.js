ad29ae0ae45e68e56b2166731364d1650a81f752
Lukas <lukasnatanael889@gmail.com>
2024-11-27 08:29:48 -0300

    criado uma playlist de teste, alterado nomes de rotas e começando redirecionamento

--- aplicação-web-nodejs/exe-03-API-de-playlists/src/controllers/playlistController.js
+++ aplicação-web-nodejs/exe-03-API-de-playlists/src/controllers/playlistController.js
@@ -1,5 +1,41 @@
+const MusicModel    = require('../models/musicModel')
+const PlaylistModel = require('../models/playlistModel')
+const Playlists     = require('../models/playlists')
+
+const playlists = new Playlists()
+
+const newPlaylist = new PlaylistModel('Academia', 'Hip Hop')
+playlists.addNewPlaylist( newPlaylist )
 class PlaylistController {
-    constructor( playlistName, musics) {
+   
+    show(req, res) {
+
+        const allPlaylists =  playlists.getAllPlaylists()
+        res.render('index', { playlists: allPlaylists })
+    }
+    
+    createPlaylist(req, res) {
+        res.render('createPlaylist')
+    }
+
+    savePlaylist(req, res) {
+        // const { name, tags } = req.body
+        const { name, tags } = req.params
+        const newPlaylist = new PlaylistModel(name, tags)
+
+        playlists.addNewPlaylist( newPlaylist )
+
+        // res.json({message: 'Playlist criada!', name: newPlaylist.name, tags: newPlaylist.tags })
+    }
+    deletePlaylist(req, res) {
+        const { id } = req.params
         
+        // playlists = playlists.forEach( playlist => playlist.id != id )
+
+        playlists.removePlaylist(id)
+
+        res.json({message: 'Playlist removida!', id: id})
     }
 }
+
+module.exports = PlaylistController
