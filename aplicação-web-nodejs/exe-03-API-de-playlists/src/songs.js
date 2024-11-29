1c2e6417c626160af9d0bce9c30c75784a8d2d58
Lukas <lukasnatanael889@gmail.com>
2024-11-29 20:09:18 -0300

    criado database de músicas e artistas

---
+++ aplicação-web-nodejs/exe-03-API-de-playlists/src/songs.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        title: 'Another Way Out',
+        clip_img:'../public/assets/images/Another Way Out.png',
+        song: '../public/assets/songs/Another_Way_Out_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Answer',
+        clip_img:'../public/assets/images/Answer.png',
+        song: '../public/assets/songs/Answer_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Better',
+        clip_img:'../public/assets/images/Better.png',
+        song: '../public/assets/songs/Better_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Jupiter & Mars',
+        clip_img:'../public/assets/images/Jupiter & Mars.png',
+        song: '../public/assets/songs/Jupiter_and_Mars_a_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Lover',
+        clip_img:'../public/assets/images/Lover.png',
+        song: '../public/assets/songs/Lover_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Quiet Place',
+        clip_img:'../public/assets/images/Quiet Place.png',
+        song: '../public/assets/songs/Square_a_Saw_-_Quiet_Place.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Meant To Be This Way',
+        clip_img:'../public/assets/images/Meant To Be This Way.png',
+        song: '../public/assets/songs/Meant_To_Be_This_Way_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Star In Sky',
+        clip_img:'../public/assets/images/Star In Sky.png',
+        song: '../public/assets/songs/Square_a_Saw_-_Star_in_the_Sky.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Wake Up',
+        clip_img:'../public/assets/images/Wake Up.png',
+        song: '../public/assets/songs/Wake_Up_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+    {
+        title: 'Where Are You Now',
+        clip_img:'../public/assets/images/Where Are You Now.png',
+        song: '../public/assets/songs/Where_Are_You_Now_-_Square_a_Saw.mp3',
+        artist_name: 'Square a Saw',
+        artist_image: '../public/assets/artists/Square a Saw.jpeg',
+        tags: [ 'futurebass', 'dance', 'pop', 'synthesizer' ]
+    },
+]
