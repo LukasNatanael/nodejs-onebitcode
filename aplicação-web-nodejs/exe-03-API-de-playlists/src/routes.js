017a45e79e9e0759eed3d2a7402777206fbf8511
Lukas <lukasnatanael889@gmail.com>
2024-12-11 08:10:52 -0300

    criado rotas para registrar e salvar novas músicas e artistas

--- aplicação-web-nodejs/exe-03-API-de-playlists/src/routes.js
+++ aplicação-web-nodejs/exe-03-API-de-playlists/src/routes.js
@@ -16,6 +16,15 @@ routes.post('/playlist/:name/:tags', playlistController.savePlaylist)
 routes.get('/playlist/:name/:tags', playlistController.show)
 routes.get('/playlist/all', playlistController.showAllPlaylists)
 
+routes.get('/playlist/music', playlistController.newMusic)
+routes.post('/playlist/music', playlistController.saveMusic)
+
+routes.get('/playlist/artists', playlistController.newArtist)
+routes.post('/playlist/artists', playlistController.saveArtist)
+
+// routes.post('/playlist/music/:imgLink/:name/:artist', playlistController.saveMusic)
+
+
 
 routes.delete('/playlist/:id', playlistController.deletePlaylist)
 
