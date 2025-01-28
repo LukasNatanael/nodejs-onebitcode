3ee635e3402d3c1d565dcb0c1cc3f32305067d3b
Lukas <lukasnatanael889@gmail.com>
2025-01-28 07:59:43 -0300

    adicionado dashboard com rota protegida

--- aplicação-web-nodejs/exe-04-API-rotas-protegidas/src/index.js
+++ aplicação-web-nodejs/exe-04-API-rotas-protegidas/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const authRouter = require('./routes/auth')
+const protectedRouter = require('./routes/protected')
 
 const app  = express()
 const PORT = process.env.PORT || 3000
 
 app.use( express.json() )
 app.use( '/auth', authRouter )
+app.use( '/protected', protectedRouter )
 
 app.listen( PORT, () => {
     console.clear()
