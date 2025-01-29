d491b20692140991ea92f894720675e6adb0e2a8
Lukas <lukasnatanael889@gmail.com>
2025-01-29 08:20:53 -0300

    migrado findUserbyEmail para o arquivo de usuários e inserido criação de token ao registrar usuário, para que ele já possa se logar

--- aplicação-web-nodejs/exe-04-API-rotas-protegidas/src/routes/auth.js
+++ aplicação-web-nodejs/exe-04-API-rotas-protegidas/src/routes/auth.js
@@ -1,36 +1,24 @@
 const express = require('express')
 const jwt     = require('jsonwebtoken')
-const users   = require('../models/users')
+const { users, findUserByEmail}   = require('../models/users')
 
 const authRouter = express.Router()
 const secretPass = '908as7d9hanye1q239eyuq80dhias'
 
-const findUserByName = (username) => {
-    const user = users.find( user => user.name === username )
-    if (!user) {
-        return res.json({ message: 'User not found' })
-    }
 
-    return user
-}
 
-const findUserByEmail = (email) => {
-    const user = users.find( user => user.email === email )
-    return user
-}
-
-// auth/login
+// POST -> auth/login
 authRouter.post('/login', (req, res) => {
     const { email, pass } = req.body
 
     const user = findUserByEmail(email)
 
-    if (!user || user.pass !== pass) {
+    if (!user || user.pass !== pass || !email || !pass) {
         return res.status(401).json({ message: 'Invalid credentials' })
     }
 
     const payload = { name: user.name, email }
-    const options = { expiresIn: '30m' }
+    const options = { expiresIn: '10m' }
 
     const token = jwt.sign( payload, secretPass, options )
 
@@ -38,20 +26,27 @@ authRouter.post('/login', (req, res) => {
     res.json({ token })
 })
 
-// auth/register
+// POST -> auth/register
 authRouter.post('/register', (req, res) => {
     const { name, email, pass } = req.body
 
     const user = findUserByEmail(email)
 
-    if (user) {
-        res.status(401).json({ message: 'Invalid credentials' })
+    if (user || !name || !email || !pass) {
+        return res.status(401).json({ message: 'Invalid credentials' })
     }
-
+    
     const newUser = { name, email, pass, position: 'standard' }
     users.push( newUser )
 
-    res.json({ message: `Hello ${name}! You have been registered successfully.`, userData: newUser})
+    const payload = { name, email }
+    const options = { expiresIn: '10m' }
+
+    const token = jwt.sign( payload, secretPass, options )
+
+    console.log( token )
+
+    res.json({ message: `Hello ${name}! You have been registered successfully.`, token: token})
 })
 
 module.exports = authRouter
